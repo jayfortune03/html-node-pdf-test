868cbb7361eb79375314bb074e02eef291e9f045
Nicholas Fortune <nfortune03@gmail.com>
2024-04-02 15:08:09 +0700

    init html

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 // const HTMLtoPDF = require("html-pdf-node");
-// const fs = require("fs");
+const fs = require("fs");
 
 // const file = { content: "<h1>Welcome to html-pdf-node</h1>" };
 // const options = { format: "A4" };
@@ -16,21 +16,15 @@
 //   });
 // });
 
-var liveServer = require("live-server");
+fs.readFile("./logo.svg", (err, data) => {
+  if (err) {
+    console.error("Error reading the file:", err);
+    return;
+  }
 
-var params = {
-  port: 8181, // Set the server port. Defaults to 8080.
-  host: "0.0.0.0", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
-  root: "/", // Set root directory that's being served. Defaults to cwd.
-  open: false, // When false, it won't load your browser by default.
-  file: "index.html", // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
-  wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec.
-  mount: [["/components", "./node_modules"]], // Mount a directory to a route.
-  logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
-  middleware: [
-    function (req, res, next) {
-      next();
-    },
-  ], // Takes an array of Connect-compatible middleware that are injected into the server middleware stack
-};
-liveServer.start(params);
+  // Convert the file data to a Base64 string
+  const base64String = data.toString("base64");
+
+  // Console log the Base64 string
+  console.log(base64String);
+});
